1049951d20df50e9f293a616dc347a549b66741f
ayonsa60 <ayonsa60@gmail.com>
2025-07-02 20:58:06 -0500

    Circle.java completed. All test in TestBoundingBox.java pass

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/DrawWidget.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/DrawWidget.java
@@ -13,14 +13,16 @@ public class DrawWidget extends View {
     private final Paint paint = new Paint();
 
     public DrawWidget(final Context context, final AttributeSet attrs, final int defStyle) {
-        this(context);
+        super(context, attrs, defStyle);
     }
 
     public DrawWidget(final Context context, final AttributeSet attrs) {
-        this(context);
+        super(context, attrs);
     }
 
-    public DrawWidget(final Context context) { super(context); }
+    public DrawWidget(final Context context) {
+        super(context);
+    }
 
     @Override
     protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
